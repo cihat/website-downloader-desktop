da2847759f6c9c25f17389d4e47399237adb4a76
Cihat Salik <cihatsalik1@gmail.com>
2022-11-09 17:40:08 +0300

    Lint && Change var to const

--- archiver/index.js
+++ archiver/index.js
@@ -1,55 +1,52 @@
-var archiver = require('archiver');
-var fs = require('fs');
+const archiver = require("archiver")
+const fs = require("fs")
 
+module.exports = (file, io, data) => {
+  const output = fs.createWriteStream("./public/sites/" + file + ".zip")
+  const archive = archiver("zip", {
+    zlib: { level: 9 }, // Sets the compression level.
+  })
 
-module.exports= (file,io,data)=>{
+  // listen for all archive data to be written
+  // 'close' event is fired only when a file descriptor is involved
+  output.on("close", function () {
+    console.log(archive.pointer() + " total bytes")
+    console.log(
+      "archiver has been finalized and the output file descriptor has closed."
+    )
+    io.emit(data.token, { progress: "Completed", file })
+  })
 
-    var output = fs.createWriteStream("./public/sites/" +file+ '.zip');
-var archive = archiver('zip', {
-  zlib: { level: 9 } // Sets the compression level.
-});
- 
-// listen for all archive data to be written
-// 'close' event is fired only when a file descriptor is involved
-output.on('close', function() {
-  console.log(archive.pointer() + ' total bytes');
-  console.log('archiver has been finalized and the output file descriptor has closed.');
-  io.emit(data.token,{progress:"Completed",file})
+  // This event is fired when the data source is drained no matter what was the data source.
+  // It is not part of this library but rather from the NodeJS Stream API.
+  // @see: https://nodejs.org/api/stream.html#stream_event_end
+  output.on("end", function () {
+    console.log("Data has been drained")
+  })
 
-});
- 
-// This event is fired when the data source is drained no matter what was the data source.
-// It is not part of this library but rather from the NodeJS Stream API.
-// @see: https://nodejs.org/api/stream.html#stream_event_end
-output.on('end', function() {
-  console.log('Data has been drained');
-});
- 
-// good practice to catch warnings (ie stat failures and other non-blocking errors)
-archive.on('warning', function(err) {
-  if (err.code === 'ENOENT') {
-    // log warning
-  } else {
-    // throw error
-    throw err;
-  }
-});
- 
-// good practice to catch this error explicitly
-archive.on('error', function(err) {
-  throw err;
-});
- 
-// pipe archive data to the file
-archive.pipe(output);
+  // good practice to catch warnings (ie stat failures and other non-blocking errors)
+  archive.on("warning", function (err) {
+    if (err.code === "ENOENT") {
+      // log warning
+    } else {
+      // throw error
+      throw err
+    }
+  })
 
-// append files from a sub-directory and naming it `new-subdir` within the archive
+  // good practice to catch this error explicitly
+  archive.on("error", function (err) {
+    throw err
+  })
 
-archive.directory('./'+file,false);
+  // pipe archive data to the file
+  archive.pipe(output)
 
-// finalize the archive (ie we are done appending files but streams have to finish yet)
-// 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
-archive.finalize();
+  // append files from a sub-directory and naming it `new-subdir` within the archive
 
- 
+  archive.directory("./" + file, false)
+
+  // finalize the archive (ie we are done appending files but streams have to finish yet)
+  // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+  archive.finalize()
 }
--- socket/socket.js
+++ socket/socket.js
@@ -1,13 +1,12 @@
 // Download full website pages.
-var wget = require('../wget');
+const wget = require("../wget")
 
-module.exports=(io)=>{
-
-    io.on('connection', function (socket) {
-        socket.on('request', function (data) {
-            console.log("Request connection received %s",data.token)
-            // now graphing the website 
-            wget(io,data)
-        });
-      });
+module.exports = (io) => {
+  io.on("connection", function (socket) {
+    socket.on("request", function (data) {
+      console.log("Request connection received %s", data.token)
+      // now graphing the website
+      wget(io, data)
+    })
+  })
 }
