8594367e66f80efc563138d5311700f104518bf9
Cihat Salik <cihatsalik1@gmail.com>
2022-11-09 17:40:08 +0300

    Lint && Change var to const

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express")
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Website downloader - Take any website offline.' });
-});
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Website downloader - Take any website offline.",
+  })
+})
 
-module.exports = router;
+module.exports = router
