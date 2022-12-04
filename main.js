322fbba72266167b6206d3ab0319c33631f751a7
Cihat Salik <cihatsalik1@gmail.com>
2022-12-05 01:24:05 +0300

    Lint

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require("electron")
 const path = require("path")
 require("./src/bin/www")
 
-let mainWindow;
+let mainWindow
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -12,28 +12,30 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true,
     },
-  });
+  })
 
-  mainWindow.loadURL("http://localhost:3000");
-  mainWindow.on("closed", function () { 
-    mainWindow = null;
-  });
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.loadURL("http://localhost:3000")
+  mainWindow.on("closed", function () {
+    mainWindow = null
+  })
 }
 
-app.on("ready", createWindow);
+app.on("ready", createWindow)
 
 app.on("resize", function (e, x, y) {
-  mainWindow.setSize(x, y);
-});
+  mainWindow.setSize(x, y)
+})
 
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on("activate", function () {
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
